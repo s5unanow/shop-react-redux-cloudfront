2317f3ddf369f6f53cfc8fe99b98fdb5f8bffc81
Oleksandr Cherevach <oleksandr_cherevach@epam.com>
2024-05-27 22:57:47 +0300

    [TASK-5] Update fronend app for importing csv

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://api-management-course-import.azure-api.net/fa-import-service-ne-001-s5una",
   bff: "https://api-management-course-products.azure-api.net/fa-products-service-ne-001-s5una",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
